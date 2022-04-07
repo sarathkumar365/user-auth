e1477fc19fba0effa0df73ccd722772e7736d911
sarath kumar <sarathkumar@pop-os.localdomain>
2022-04-07 12:39:29 +0530

    iniial setupcomplete

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const port = 4444;
+
+mongoose.connect("mongodb://localhost:27017/users").then(() => {
+  console.log(`conneccted to DATABASE`);
+});
+
+app.listen(port, () => {
+  console.log(`started listening in ${port}`);
+});
