634adbe094480fea33ef8a649bc9212b1d79e57d
sarath kumar <sarathkumark.s365@gmail.com>
2022-06-01 11:09:34 +0530

    updates

--- server.js
+++ server.js
@@ -10,10 +10,19 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/user-auth').then(() => {
-  // eslint-disable-next-line no-console
-  console.log(`conneccted to DATABASE`);
-});
+// mongoose.connect('mongodb://localhost:27017/user-auth').then(() => {
+//   // eslint-disable-next-line no-console
+//   console.log(`conneccted to DATABASE`);
+// });
+
+mongoose
+  .connect(
+    `mongodb+srv://sarathkumar:alchemist%401@user-auth.8xlpxlq.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log(`conneccted to DATABASE`);
+  });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
